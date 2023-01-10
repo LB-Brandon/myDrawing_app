14d595acc07b53b96fb882825f966eb82e8a151f
LB-Brandon <rmsdud623@gmail.com>
2023-01-10 11:12:50 +0900

    Feat 초기 설정
    1. 세로 모드 고정 - AndroidManifest.xml : android:screenOrientation="portrait"
    2. DrawingView.kt 생성 및 초기화 함수 생성 - setUpDrawing()

---
+++ app/src/main/java/com/branden/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,46 @@
+package com.branden.kidsdrawingapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+
+//    메인 엑티비티에 띄울 뷰를 위한 클래스 (액티비티가 아님)
+//    무언가를 그리려면 뷰를 사용해야 함
+class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs) {
+
+    private var mDrawPath : CustomPath? = null
+    private var mCanvasBitmap : Bitmap? = null
+    private var mDrawPaint : Paint? = null
+    private var mCanvasPaint : Paint? = null
+    private var mBrushSize : Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas : Canvas? = null
+
+    init {
+           setUpDrawing()
+    }
+
+    private fun setUpDrawing(){
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+    }
+
+
+    internal inner class CustomPath(var color : Int, var brushThickness : Float) : Path(){
+
+    }
+
+
+}
