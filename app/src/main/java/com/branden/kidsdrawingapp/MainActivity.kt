7a9a070cd1dd229a0bbdb5dffa09820e25546d9b
LB-Brandon <rmsdud623@gmail.com>
2023-01-11 14:49:36 +0900

    Feat 이미지 저장 진행중

--- app/src/main/java/com/branden/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/branden/kidsdrawingapp/MainActivity.kt
@@ -3,11 +3,18 @@ package com.branden.kidsdrawingapp
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -17,42 +24,70 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
 
-    val openGalleryLauncher : ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-            if(result.resultCode == RESULT_OK && result.data != null){
-                val imageBackGround : ImageView = findViewById(R.id.iv_background)
+//    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//            if (result.resultCode == RESULT_OK && result.data != null) {
+//                val imageBackGround: ImageView = findViewById(R.id.iv_background)
+//
+//                Log.i("gallery", "정상 작동")
+//                imageBackGround.setImageURI(result.data?.data)
+//            }else{
+//                Log.i("gallery", "비 정상 작동 $result")
+//            }
+//        }
 
-                imageBackGround.setImageURI(result.data?.data)
-            }
+    val openGalleryLauncher:ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+        //Todo 3: get the returned result from the lambda and check the resultcode and the data returned
+        if (result.resultCode == RESULT_OK && result.data != null){
+            //process the data
+            //Todo 4 if the data is not null reference the imageView from the layout
+            Log.i("gallery", "정상 작동")
+            val imageBackground:ImageView = findViewById(R.id.iv_background)
+            //Todo 5: set the imageuri received
+            imageBackground.setImageURI(result.data?.data)
+        }else{
+            Log.i("gallery", "비 정상 작동 $result")
         }
+    }
 
     private val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
             permissions.entries.forEach {
-                val permissionName = it.key
+                val perMissionName = it.key
                 val isGranted = it.value
 
-                if(isGranted){
-                    Toast.makeText(this@MainActivity,
-                    "Permission granted now you can read the storage files.",
-                    Toast.LENGTH_LONG).show()
+                if (isGranted) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission granted now you can read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
 
-                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    val pickIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
 
                     openGalleryLauncher.launch(pickIntent)
 
-                }else{
-                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
-                        Toast.makeText(this@MainActivity,
+                } else {
+                    if (perMissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            this@MainActivity,
                             "Permission denied now you can read the storage files.",
-                            Toast.LENGTH_LONG).show()
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
                 }
             }
@@ -79,36 +114,57 @@ class MainActivity : AppCompatActivity() {
             showBrushSizeChooserDialog()
         }
 
-        val ibGallery : ImageButton = findViewById(R.id.ib_gallery)
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
         ibGallery.setOnClickListener {
             requestStoragePermission()
         }
 
-        val ibUndo : ImageButton = findViewById(R.id.ib_undo)
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
         ibUndo.setOnClickListener {
             drawingView?.onClickUndo()
         }
 
-        val ibRedo : ImageButton = findViewById(R.id.ib_redo)
+        val ibRedo: ImageButton = findViewById(R.id.ib_redo)
         ibRedo.setOnClickListener {
             drawingView?.onClickRedo()
         }
 
+        val ibSave: ImageButton = findViewById(R.id.ib_save)
+        ibSave.setOnClickListener {
+            if(isReadStorageAllowed()){
+                lifecycleScope.launch{
+                    val flDrawingView : FrameLayout = findViewById(R.id.fl_drawing_view_container)
+                    val myBitmap : Bitmap = getBitmapFromView(flDrawingView)
+                    saveBitmapFile(myBitmap)
+                }
+            }
+        }
+    }
 
+    private fun isReadStorageAllowed() : Boolean {
+        val result = ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
     }
 
     private fun requestStoragePermission() {
-        if(ActivityCompat.shouldShowRequestPermissionRationale(
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
-                Manifest.permission.READ_EXTERNAL_STORAGE))
-        {
-            showRationalDialog("Kids Drawing App",
-                "Kids Drawing App " + "needs to Access your External Storage")
-        }else{
-          requestPermission.launch(arrayOf(
-              Manifest.permission.READ_EXTERNAL_STORAGE
-          // TODO - Add writing external storage permission
-          ))
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationalDialog(
+                "Kids Drawing App",
+                "Kids Drawing App " + "needs to Access your External Storage"
+            )
+        } else {
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
         }
     }
 
@@ -131,6 +187,7 @@ class MainActivity : AppCompatActivity() {
             drawingView!!.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
+
         brushDialog.show()
     }
 
@@ -161,6 +218,56 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         builder.create().show()
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        // 캔버스에 배경 추가
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        // 캔버스에 나의 그림 추가
+        view.draw(canvas)
+
+        return returnedBitmap
+    }
+
+    private suspend fun saveBitmapFile(mBitmap : Bitmap?) : String {
+        var result = ""
+        withContext(Dispatchers.IO){
+            if(mBitmap != null){
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+                    val f = File(externalCacheDir?.absoluteFile.toString() + File.separator + "KidDrawingApp_" + System.currentTimeMillis() / 1000 + ".png")
+
+                    val fo = FileOutputStream(f)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+
+                    result = f.absolutePath
+
+                    runOnUiThread {
+                        if(result.isNotEmpty()){
+                            Toast.makeText(this@MainActivity,
+                                "File saved successfully : $result", Toast.LENGTH_LONG).show()
+                        }else{
+                            Toast.makeText(this@MainActivity,
+                                "Something went wrong!!!", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }catch (e: Exception){
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
     }
 
 }
